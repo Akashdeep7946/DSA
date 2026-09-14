e913c4e874ac40bf48639484e6bc7046d0762e5b
Akashdeep7946 <11100akashdeep@gmail.com>
2026-09-14 23:37:23 +0530

    Time: 1 ms (44.76%) | Memory: 263.5 MB (18.17%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -2,16 +2,13 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        k = k % n;
-
-        vector<int> temp(n);
-
-        for (int i = 0; i < n; i++) {
-            temp[(i + k) % n] = nums[i];
-        }
-
-        for (int i = 0; i < n; i++) {
-            nums[i] = temp[i];
-        }
+        k = k % n; // Handle cases where k > n
+        
+        // 1. Reverse the entire array
+        reverse(nums.begin(), nums.end());
+        // 2. Reverse the first k elements
+        reverse(nums.begin(), nums.begin() + k);
+        // 3. Reverse the remaining n - k elements
+        reverse(nums.begin() + k, nums.end());
     }
 };
